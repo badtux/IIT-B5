d6dde5ebffeb8e48e6fa22fae6048b3ac0416a6f
rison-github <risonperera2006@gmail.com>
2023-11-18 15:36:19 +0530

    Node JS Prog

---
+++ rison-github/app.js
@@ -0,0 +1,8 @@
+// app.js
+const mathOperations = require('./arithmetic');
+const num1 = 10;
+const num2 = 5;
+console.log( 'Addition: ${mathOperations.add(num1. num2)}' );
+console.log( 'Addition: ${mathOperations.subtract(num1. num2)}' );
+console.log( 'Addition: ${mathOperations.multiply(num1. num2)}' );
+console.log( 'Addition: ${mathOperations.divide(num1. num2)}' );
