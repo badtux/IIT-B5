05bbfc9b2dfc78849d867b311e33c214638986db
badtux <viraj@ceynet.asia>
2023-11-25 17:04:50 +0530

    added db

--- node/math.js
+++ node/math.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 const math = require('./arith');
 
 const n1 = 10;
@@ -6,9 +8,27 @@ const n2 = 20;
 
 //console.log(`Sum is ${math.add(n1, n2)}`);
 
+const mydb = new sqlite3.Database('campus.db', (err) => {
+    if(err){
+        console.error('Error starting db '+err.message);
+    }
+    else {
+        mydb.run(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                country TEXT
+            )`
+        );
+    }
+});
+
 const myapp = express();
 
+myapp.set('view engine', 'ejs');
+myapp.set('views', 'views');
 myapp.use(express.static('public'));
+myapp.use(bodyParser.urlencoded({ extended: true }));
 
 myapp.post('/calc/add', (req,res) => {
     res.json({
@@ -17,7 +37,33 @@ myapp.post('/calc/add', (req,res) => {
 });
 
 myapp.get('/', (req,res) =>{
+    res.send('<html><body><h1>Hello '+req.query.name+'!</h1></body></html>');
+});
 
+myapp.get('/students/199', (req,res) => {
+    res.render('student', { 
+        fullName: 'Barak Obama',
+        country: 'US',
+        status: 'Retired'
+    });
+});
+
+myapp.get('/register', (req,res) => {
+    res.render('signup');
+});
+
+myapp.post('/signup', (req,res) => {
+//    console.log(req.body); 
+   const { name, country } = req.body; 
+   mydb.run('INSERT INTO users (name, country) VALUES (?, ?)', [name,country], (err) => {
+        if (err) {
+        // console.error('Error inserting data into the database:', err.message);
+            res.send('Error submitting the form. '+err.message);
+        } else {
+            // console.log('Data inserted successfully');
+            res.send('Form submitted successfully!');
+        }
+    });
 });
 
 myapp.delete('/calc/add', (req,res) => {
